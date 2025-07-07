9983be0892961e45d423e24507086c83fa8fffd3
aswinnarayanan <aswinn89@gmail.com>
2025-07-07 10:59:17 +0000

    Remove unused scripts

--- .github/workflows/upload-nectar.sh
+++
@@ -1,8 +0,0 @@
-echo "[DEBUG] Attempting upload to Nectar Object Storage:"
-
-IMAGENAME=$1
-export IMAGE_HOME="/storage/tmp"
-
-
-time rclone copy $IMAGE_HOME/${IMAGENAME}_${BUILDDATE}.simg nectar:/neurodesk/temporary-builds-new
-echo "[DEBUG] Done with uploading to Nectar Object Storage!"
