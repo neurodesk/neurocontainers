778118ce8e66e2eee5adfb93983a4350c17cec07
Steffen Bollmann <stebo85@users.noreply.github.com>
2025-11-21 17:02:09 -0800

    cleanup

--- recipes/musclemap/musclemap.py
+++ recipes/musclemap/musclemap.py
@@ -111,24 +111,6 @@ def process(connection, config, metadata):
             else:
                 logging.error("Unsupported data type %s", type(item).__name__)
 
-        # Extract raw ECG waveform data. Basic sorting to make sure that data
-        # is time-ordered, but no additional checking for missing data.
-        # ecgData has shape (5 x timepoints)
-        if len(waveformGroup) > 0:
-            waveformGroup.sort(key=lambda item: item.time_stamp)
-            ecgData = [item.data for item in waveformGroup if item.waveform_id == 0]
-            ecgData = np.concatenate(ecgData, 1)
-
-        # Process any remaining groups of raw or image data.  This can
-        # happen if the trigger condition for these groups are not met.
-        # This is also a fallback for handling image data, as the last
-        # image in a series is typically not separately flagged.
-        if len(acqGroup) > 0:
-            logging.info("Processing a group of k-space data (untriggered)")
-            image = process_raw(acqGroup, connection, config, metadata)
-            connection.send_image(image)
-            acqGroup = []
-
         if len(imgGroup) > 0:
             logging.info("Processing a group of images (untriggered)")
             image = process_image(imgGroup, connection, config, metadata)
@@ -143,123 +125,6 @@ def process(connection, config, metadata):
         connection.send_close()
 
 
-def process_raw(group, connection, config, metadata):
-    if len(group) == 0:
-        return []
-
-    # Start timer
-    tic = perf_counter()
-
-    # Create folder, if necessary
-    if not os.path.exists(debugFolder):
-        os.makedirs(debugFolder)
-        logging.debug("Created folder " + debugFolder + " for debug output files")
-
-    # Format data into single [cha PE RO phs] array
-    lin = [acquisition.idx.kspace_encode_step_1 for acquisition in group]
-    phs = [acquisition.idx.phase for acquisition in group]
-
-    # Use the zero-padded matrix size
-    data = np.zeros(
-        (
-            group[0].data.shape[0],
-            metadata.encoding[0].encodedSpace.matrixSize.y,
-            metadata.encoding[0].encodedSpace.matrixSize.x,
-            max(phs) + 1,
-        ),
-        group[0].data.dtype,
-    )
-
-    rawHead = [None] * (max(phs) + 1)
-
-    for acq, lin, phs in zip(group, lin, phs):
-        if (lin < data.shape[1]) and (phs < data.shape[3]):
-            # TODO: Account for asymmetric echo in a better way
-            data[:, lin, -acq.data.shape[1] :, phs] = acq.data
-
-            # center line of k-space is encoded in user[5]
-            if (rawHead[phs] is None) or (
-                np.abs(acq.getHead().idx.kspace_encode_step_1 - acq.getHead().idx.user[5])
-                < np.abs(rawHead[phs].idx.kspace_encode_step_1 - rawHead[phs].idx.user[5])
-            ):
-                rawHead[phs] = acq.getHead()
-
-    # Flip matrix in RO/PE to be consistent with ICE
-    data = np.flip(data, (1, 2))
-
-    logging.debug("Raw data is size %s" % (data.shape,))
-    np.save(debugFolder + "/" + "raw.npy", data)
-
-    # Fourier Transform
-    data = fft.fftshift(data, axes=(1, 2))
-    data = fft.ifft2(data, axes=(1, 2))
-    data = fft.ifftshift(data, axes=(1, 2))
-    data *= np.prod(data.shape)  # FFT scaling for consistency with ICE
-
-    # Sum of squares coil combination
-    # Data will be [PE RO phs]
-    data = np.abs(data)
-    data = np.square(data)
-    data = np.sum(data, axis=0)
-    data = np.sqrt(data)
-
-    logging.debug("Image data is size %s" % (data.shape,))
-    np.save(debugFolder + "/" + "img.npy", data)
-
-    # Remove readout oversampling
-    offset = int((data.shape[1] - metadata.encoding[0].reconSpace.matrixSize.x) / 2)
-    data = data[:, offset : offset + metadata.encoding[0].reconSpace.matrixSize.x]
-
-    # Remove phase oversampling
-    offset = int((data.shape[0] - metadata.encoding[0].reconSpace.matrixSize.y) / 2)
-    data = data[offset : offset + metadata.encoding[0].reconSpace.matrixSize.y, :]
-
-    logging.debug("Image without oversampling is size %s" % (data.shape,))
-    np.save(debugFolder + "/" + "imgCrop.npy", data)
-
-    # Measure processing time
-    toc = perf_counter()
-    strProcessTime = "Total processing time: %.2f ms" % ((toc - tic) * 1000.0)
-    logging.info(strProcessTime)
-
-    # Send this as a text message back to the client
-    connection.send_logging(constants.MRD_LOGGING_INFO, strProcessTime)
-
-    # Format as ISMRMRD image data
-    imagesOut = []
-    for phs in range(data.shape[2]):
-        # Create new MRD instance for the processed image
-        # data has shape [PE RO phs], i.e. [y x].
-        # from_array() should be called with 'transpose=False' to avoid warnings, and when called
-        # with this option, can take input as: [cha z y x], [z y x], or [y x]
-        tmpImg = ismrmrd.Image.from_array(data[..., phs], transpose=False)
-
-        # Set the header information
-        tmpImg.setHead(mrdhelper.update_img_header_from_raw(tmpImg.getHead(), rawHead[phs]))
-        tmpImg.field_of_view = (
-            ctypes.c_float(metadata.encoding[0].reconSpace.fieldOfView_mm.x),
-            ctypes.c_float(metadata.encoding[0].reconSpace.fieldOfView_mm.y),
-            ctypes.c_float(metadata.encoding[0].reconSpace.fieldOfView_mm.z),
-        )
-        tmpImg.image_index = phs
-
-        # Set ISMRMRD Meta Attributes
-        tmpMeta = ismrmrd.Meta()
-        tmpMeta["DataRole"] = "Image"
-        tmpMeta["ImageProcessingHistory"] = ["FIRE", "PYTHON"]
-        tmpMeta["Keep_image_geometry"] = 1
-
-        xml = tmpMeta.serialize()
-        # logging.debug("Image MetaAttributes: %s", xml)
-        tmpImg.attribute_string = xml
-        imagesOut.append(tmpImg)
-
-    # Call process_image() to invert image contrast
-    imagesOut = process_image(imagesOut, connection, config, metadata)
-
-    return imagesOut
-
-
 def process_image(images, connection, config, metadata):
     if len(images) == 0:
         return []
@@ -269,9 +134,9 @@ def process_image(images, connection, config, metadata):
         os.makedirs(debugFolder)
         logging.debug("Created folder " + debugFolder + " for debug output files")
 
-    logging.debug(
-        "Processing data with %d images of type %s", len(images), images[0].data_dtype
-    )
+    # logging.debug(
+    #     "Processing data with %d images of type %s", len(images), ismrmrd.get_dtype_from_data_type(images[0].data_type)
+    # )
 
     # Note: The MRD Image class stores data as [cha z y x]
 
@@ -288,29 +153,13 @@ def process_image(images, connection, config, metadata):
     # Reformat data to [y x img cha z], i.e. [row ~col] for the first two dimensions
     data = data.transpose((3, 4, 0, 1, 2))
 
-    # Display MetaAttributes for first image
-    # logging.debug("MetaAttributes[0]: %s", ismrmrd.Meta.serialize(meta[0]))
-
-    # Optional serialization of ICE MiniHeader
-    # if 'IceMiniHead' in meta[0]:
-    #     logging.debug("IceMiniHead[0]: %s", base64.b64decode(meta[0]['IceMiniHead']).decode('utf-8'))
-
-    # logging.debug("Stebo: Original image data is size %s" % (data.shape,))
-    # # e.g. gre with 128x128x10 with phase and magnitude results in [128 128 1 1 1]
-    # np.save(debugFolder + "/" + "imgOrig.npy", data)
-
     # convert data to nifti using nibabel
-
     xform = np.eye(4)
     new_img = nib.nifti1.Nifti1Image(np.squeeze(data), xform)
     nib.save(new_img, "/buildhostdirectory/input.nii.gz")
 
-    # Run preprocess.py
+    # Run mm_segment
     preprocess_result = subprocess.run(["mm_segment", "-i", "/buildhostdirectory/input.nii.gz"], check=True)
-
-    # If preprocess.py ran successfully, run test_nifti.py
-    # if preprocess_result.returncode == 0:
-    #    subprocess.run(['python', '/synCT/src/test_nifti.py'], check=True)
 
     img = nib.load("input_dseg.nii.gz")
     data = img.get_fdata()
@@ -344,11 +193,6 @@ def process_image(images, connection, config, metadata):
         data *= maxVal / data.max()
         data = np.around(data)
         data = data.astype(np.int16)
-
-    # Invert image contrast
-    # data = maxVal-data
-    # data = np.abs(data)
-    # np.save(debugFolder + "/" + "imgInverted.npy", data)
 
     currentSeries = 0
 
@@ -395,16 +239,6 @@ def process_image(images, connection, config, metadata):
         tmpMeta["SequenceDescriptionAdditional"] = "OpenRecon"
         tmpMeta["Keep_image_geometry"] = 1
 
-        # if ("parameters" in config) and ("options" in config["parameters"]):
-        #     # Example for sending ROIs
-        #     if config["parameters"]["options"] == "roi":
-        #         logging.info("Creating ROI_example")
-        #         tmpMeta["ROI_example"] = create_example_roi(data.shape)
-
-            # # Example for setting colormap
-            # if config["parameters"]["options"] == "colormap":
-            #     tmpMeta["LUTFileName"] = "MicroDeltaHotMetal.pal"
-
         # Add image orientation directions to MetaAttributes if not already present
         if tmpMeta.get("ImageRowDir") is None:
             tmpMeta["ImageRowDir"] = [
@@ -429,22 +263,4 @@ def process_image(images, connection, config, metadata):
     return imagesOut
 
 
-# Create an example ROI <3
-# def create_example_roi(img_size):
-#     t = np.linspace(0, 2 * np.pi)
-#     x = 16 * np.power(np.sin(t), 3)
-#     y = -13 * np.cos(t) + 5 * np.cos(2 * t) + 2 * np.cos(3 * t) + np.cos(4 * t)
 
-#     # Place ROI in bottom right of image, offset and scaled to 10% of the image size
-#     x = (x - np.min(x)) / (np.max(x) - np.min(x))
-#     y = (y - np.min(y)) / (np.max(y) - np.min(y))
-#     x = (x * 0.08 * img_size[0]) + 0.82 * img_size[0]
-#     y = (y * 0.10 * img_size[1]) + 0.80 * img_size[1]
-
-#     rgb = (1, 0, 0)  # Red, green, blue color -- normalized to 1
-#     thickness = 1  # Line thickness
-#     style = 0  # Line style (0 = solid, 1 = dashed)
-#     visibility = 1  # Line visibility (0 = false, 1 = true)
-
-#     roi = mrdhelper.create_roi(x, y, rgb, thickness, style, visibility)
-#     return roi
